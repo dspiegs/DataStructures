df9e8809dc6f9e7739426bbd0b866da355397c25
dspiegs <dspiegs@dspiegs.com>
2015-07-17 08:14:09 -0700

    Added Insertion Sort

---
+++ Algoritms.Test/InsertionSortTest.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using Algoritms;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Algorithms.Test
+{
+    [TestClass]
+    public class InsertionSortTest
+    {
+        [TestMethod]
+        public void Sort()
+        {
+            int[] a = {3, 2, 1};
+            a = a.InsertionSort();
+            Assert.AreEqual(1, a[0]);
+            Assert.AreEqual(2, a[1]);
+            Assert.AreEqual(3, a[2]);
+        }
+    }
+}
---
+++ Algoritms/MergeSort.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algoritms
+{
+    public static class Sorting
+    {
+        public static T[] InsertionSort<T>(this T[] array) where T : IComparable<T>
+        {
+            if (array == null)
+            {
+                return null;
+            }
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                int j = i;
+                while (j > 0 && array[j - 1].CompareTo(array[j]) > 0)
+                {
+                    T temp = array[j];
+                    array[j] = array[j - 1];
+                    array[j - 1] = temp;
+                    j--;
+                }
+            }
+
+            return array;
+        }
+    }
+}
